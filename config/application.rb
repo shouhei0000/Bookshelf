06feae6bc319e6f4ec3b240b269471b73ab02824
shouhei0000 <shouhei19@icloud.com>
2020-09-09 00:27:17 +0900

    top page

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,10 @@ module Yourbookshelf
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.generators do |g|
+      g.assets false          # CSS, JavaScriptファイル生成せず
+      g.skip_routes false     # trueならroutes.rb変更せず、falseなら通常通り変更
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
